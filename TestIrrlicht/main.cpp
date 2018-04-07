95688c4076774f76a5da35328dcaa128e1d26eb8
Jean-Baptiste Balmeyer <balmeyer@gmail.com>
2018-04-07 02:23:03 +0200

    try scene

--- TestIrrlicht/main.cpp
+++ TestIrrlicht/main.cpp
@@ -26,7 +26,7 @@ int main() {
 		createDevice(
 			video::EDT_DIRECT3D9,	//deviceType: Type of the device. This can currently be the Null-device, one of the two software renderers, D3D8, D3D9, or OpenGL. In this example we use EDT_SOFTWARE, but to try out, you might want to change it to EDT_BURNINGSVIDEO, EDT_NULL, EDT_DIRECT3D8, EDT_DIRECT3D9, or EDT_OPENGL.
 			dimension2d<u32>(1900, 900), //windowSize: Size of the Window or screen in FullScreenMode to be created. In this example we use 640x480.
-			64, //bits: Amount of color bits per pixel.This should be 16 or 32. The parameter is often ignored when running in windowed mode.
+			32, //bits: Amount of color bits per pixel.This should be 16 or 32. The parameter is often ignored when running in windowed mode.
 			false,	//fullscreen: Specifies if we want the device to run in fullscreen mode or not.
 			false,	//stencilbuffer: Specifies if we want to use the stencil buffer (for drawing shadows).
 			false,	//vsync : Specifies if we want to have vsync enabled, this is only useful in fullscreen mode.
@@ -49,8 +49,10 @@ int main() {
 
 	IGUIButton* button = guienv->addButton(recti(10, 30, 50, 60), 0, 0, L"Test !!! Mdr");
 
+	
+
 	//
-	SKeyMap keyMap[8];
+	SKeyMap keyMap[9];
 	keyMap[0].Action = EKA_MOVE_FORWARD;
 	keyMap[0].KeyCode = KEY_UP;
 	keyMap[1].Action = EKA_MOVE_FORWARD;
@@ -71,16 +73,20 @@ int main() {
 	keyMap[7].Action = EKA_STRAFE_RIGHT;
 	keyMap[7].KeyCode = KEY_KEY_D;
 
+	//jump ?
+	keyMap[8].Action = EKA_JUMP_UP;
+	keyMap[8].KeyCode = KEY_SPACE;
+
 
 	//add camera
 	ICameraSceneNode* camera = smgr->addCameraSceneNodeFPS(0, 
-		100, //roateSpeed
-		0.01f, //movespeed
+		70, //roateSpeed
+		0.02f, //movespeed
 		-1,	//id
 		keyMap, //key map
-		8,	//mapsize
+		9,	//mapsize
 		true, //noVerticalMovement ,
-		0.5f, //jump speed
+		0.03f, //jump speed
 		false, //invert mouse
 		true //make active
 		
@@ -93,18 +99,28 @@ int main() {
 	ITexture* textureMur = driver->getTexture("Img/mur.png");
 	ITexture* textureSol = driver->getTexture("Img/dry.png");
 
-	//mesh ground
-	
-	IAnimatedMesh* meshSol = smgr->addHillPlaneMesh("sol", dimension2df(1, 1), dimension2du(64, 64), 0, 0, dimension2df(0,0),dimension2df(64,64));
-	IMeshSceneNode* sol = smgr->addMeshSceneNode(meshSol);
-	sol->setPosition(vector3df(0, -0.5, 0));
-	sol->setMaterialFlag(E_MATERIAL_FLAG::EMF_LIGHTING, false);
-	sol->setMaterialTexture(0, textureSol);
+
 
 	//add a cube
-	IMeshSceneNode* cube = smgr->addCubeSceneNode(1, 0, 0, vector3df(0, 0, 0));
+	IMeshSceneNode* cube = smgr->addCubeSceneNode(1, 0, 0, vector3df(-16, 0, -16));
 	cube->setMaterialFlag(E_MATERIAL_FLAG::EMF_LIGHTING, false);
 	cube->setMaterialTexture(0, textureMur);
+
+	IMeshSceneNode* cube2 = smgr->addCubeSceneNode(1, 0, 0, vector3df(16, 0, -16));
+	cube2->setMaterialFlag(E_MATERIAL_FLAG::EMF_LIGHTING, false);
+	cube2->setMaterialTexture(0, textureMur);
+
+	IMeshSceneNode* cube3 = smgr->addCubeSceneNode(1, 0, 0, vector3df(16, 0, 16));
+	cube3->setMaterialFlag(E_MATERIAL_FLAG::EMF_LIGHTING, false);
+	cube3->setMaterialTexture(0, textureMur);
+
+	IMeshSceneNode* cube4 = smgr->addCubeSceneNode(1, 0, 0, vector3df(-16, 0, 16));
+	cube4->setMaterialFlag(E_MATERIAL_FLAG::EMF_LIGHTING, false);
+	cube4->setMaterialTexture(0, textureMur);
+
+	IMeshSceneNode* cube5 = smgr->addCubeSceneNode(1, 0, 0, vector3df(0, 0, 0));
+	cube5->setMaterialFlag(E_MATERIAL_FLAG::EMF_LIGHTING, false);
+	cube5->setMaterialTexture(0, textureMur);
 
 	//animator
 	ISceneNodeAnimator* animator = smgr->createRotationAnimator(vector3df(0, 0.1f, 0));
@@ -120,21 +136,51 @@ int main() {
 	ISceneCollisionManager* collManager = smgr->getSceneCollisionManager();
 
 	//test terrain
-	ITerrainSceneNode* terrain = smgr->addTerrainSceneNode("Img/terrain.bmp" ,
-		0,
-		-1,
-		vector3df(0,0,0),
-		vector3df(0, 0, 0),
-		vector3df(32.f, 1.f, 32.f),
-		SColor(128,100,155,255),
+	/*
+	ITerrainSceneNode* terrain = smgr->addTerrainSceneNode("Img/t64.png" ,	//map
+		0,	//parent
+		-1,	//id
+		vector3df(-16,-2,-16),		//position
+		vector3df(0, 0, 0),		//rotation
+		vector3df(4.f, 1.f/2.f, 4.f),	//scale
+		SColor(255, 255, 255, 255),
 		5,
 		ETPS_17,
-		4
+		0	//smooth factor
 	);
+	terrain->scaleTexture(32.0f ,32.0f);
 	terrain->setMaterialFlag(E_MATERIAL_FLAG::EMF_LIGHTING, false);
 	terrain->setMaterialTexture(0, textureSol);
-	terrain->setMaterialType(E_MATERIAL_TYPE::EMT_DETAIL_MAP);
-	terrain->scaleTexture(1.f, 20.f);
+	
+	terrain->setMaterialType(E_MATERIAL_TYPE::EMT_DETAIL_MAP); */
+
+	//test obj
+	IAnimatedMesh* meshBlob = smgr->getMesh("Map/blob.obj");
+	ISceneNode * complix = smgr->addMeshSceneNode(meshBlob);
+	complix->setPosition(vector3df(0, 0, 0));
+	complix->setMaterialFlag(E_MATERIAL_FLAG::EMF_LIGHTING, false);
+	//complix->setMaterialTexture(0, textureMur);
+	//complix->setScale(vector3df(1, 1, 1) / 16);
+
+	scene::IMeshSceneNode* q3node = 0;
+	q3node = smgr->addOctreeSceneNode(meshBlob);
+	ITriangleSelector* selector = smgr->createOctreeTriangleSelector(
+		meshBlob, q3node, 128);
+	q3node->setTriangleSelector(selector);
+
+	scene::ISceneNodeAnimator* anim = smgr->createCollisionResponseAnimator(selector,
+		camera,
+		core::vector3df(1, 1, 1),	//size of body (camera)
+		core::vector3df(0, -0.2f, 0),	//gravity
+		core::vector3df(0, 1, 0)	//position
+	);
+
+	camera->addAnimator(anim);
+	anim->drop();
+	selector->drop();
+
+	device->getCursorControl()->setVisible(false);
+
 
 	while (device->run()) {
 		//tmp
