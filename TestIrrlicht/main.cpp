b185ee1868e1e65f092be8b6eb7cf1ab2d3ef633
Jean-Baptiste Balmeyer <balmeyer@gmail.com>
2018-04-08 18:16:30 +0200

    dev dev

--- TestIrrlicht/main.cpp
+++ TestIrrlicht/main.cpp
@@ -21,13 +21,13 @@ int main() {
 	//http://irrlicht.sourceforge.net/docu/example001.html
 
 	EventReceiver receiver;
-
+	              
 	IrrlichtDevice *device =
 		createDevice(
 			video::EDT_DIRECT3D9,	//deviceType: Type of the device. This can currently be the Null-device, one of the two software renderers, D3D8, D3D9, or OpenGL. In this example we use EDT_SOFTWARE, but to try out, you might want to change it to EDT_BURNINGSVIDEO, EDT_NULL, EDT_DIRECT3D8, EDT_DIRECT3D9, or EDT_OPENGL.
-			dimension2d<u32>(1900, 900), //windowSize: Size of the Window or screen in FullScreenMode to be created. In this example we use 640x480.
+			dimension2d<u32>(1920, 1080), //windowSize: Size of the Window or screen in FullScreenMode to be created. In this example we use 640x480.
 			32, //bits: Amount of color bits per pixel.This should be 16 or 32. The parameter is often ignored when running in windowed mode.
-			false,	//fullscreen: Specifies if we want the device to run in fullscreen mode or not.
+			true,	//fullscreen: Specifies if we want the device to run in fullscreen mode or not.
 			false,	//stencilbuffer: Specifies if we want to use the stencil buffer (for drawing shadows).
 			false,	//vsync : Specifies if we want to have vsync enabled, this is only useful in fullscreen mode.
 			0);		//eventReceiver: An object to receive events. We do not want to use this parameter here, and set it to 0.
@@ -44,12 +44,19 @@ int main() {
 	//register scene manager
 	receiver.setSceneManager(smgr);
 
-	guienv->addStaticText(L"Hello World! This is the Irrlicht Software renderer!",
-		rect<s32>(10, 10, 260, 22), true);
+	//skybox
+	ISceneNode * skybox = smgr->addSkyBoxSceneNode(
+		driver->getTexture("Img/hw_lagoon/lagoon_up.tga"),	//top
+		driver->getTexture("Img/hw_lagoon/lagoon_dn.tga") ,	//bottom
+		//
+		driver->getTexture("Img/hw_lagoon/lagoon_rt.tga") ,//left ok
+		driver->getTexture("Img/hw_lagoon/lagoon_lf.tga") ,//right
+		
+		driver->getTexture("Img/hw_lagoon/lagoon_ft.tga"), // front ok
+		driver->getTexture("Img/hw_lagoon/lagoon_bk.tga") //back 
+		
+	);
 
-	IGUIButton* button = guienv->addButton(recti(10, 30, 50, 60), 0, 0, L"Test !!! Mdr");
-
-	
 
 	//
 	SKeyMap keyMap[9];
@@ -91,74 +98,36 @@ int main() {
 		true //make active
 		
 		);
-	camera->setNearValue(0.1);
+	camera->setNearValue(0.1f);
 	camera->setPosition(vector3df(4, 0, 0));
 
+	//fog
+	driver->setFog(SColor(128, 32, 32,32), E_FOG_TYPE::EFT_FOG_LINEAR, 80.f, 300.0f, 0.05f, false);
 
 	//texture wall
 	ITexture* textureMur = driver->getTexture("Img/mur.png");
 	ITexture* textureSol = driver->getTexture("Img/dry.png");
 
+	//test scene irrlicht
+	//bool loaded = smgr->loadScene("scene.irr");
 
-
-	//add a cube
-	IMeshSceneNode* cube = smgr->addCubeSceneNode(1, 0, 0, vector3df(-16, 0, -16));
-	cube->setMaterialFlag(E_MATERIAL_FLAG::EMF_LIGHTING, false);
-	cube->setMaterialTexture(0, textureMur);
-
-	IMeshSceneNode* cube2 = smgr->addCubeSceneNode(1, 0, 0, vector3df(16, 0, -16));
-	cube2->setMaterialFlag(E_MATERIAL_FLAG::EMF_LIGHTING, false);
-	cube2->setMaterialTexture(0, textureMur);
-
-	IMeshSceneNode* cube3 = smgr->addCubeSceneNode(1, 0, 0, vector3df(16, 0, 16));
-	cube3->setMaterialFlag(E_MATERIAL_FLAG::EMF_LIGHTING, false);
-	cube3->setMaterialTexture(0, textureMur);
-
-	IMeshSceneNode* cube4 = smgr->addCubeSceneNode(1, 0, 0, vector3df(-16, 0, 16));
-	cube4->setMaterialFlag(E_MATERIAL_FLAG::EMF_LIGHTING, false);
-	cube4->setMaterialTexture(0, textureMur);
-
-	IMeshSceneNode* cube5 = smgr->addCubeSceneNode(1, 0, 0, vector3df(0, 0, 0));
-	cube5->setMaterialFlag(E_MATERIAL_FLAG::EMF_LIGHTING, false);
-	cube5->setMaterialTexture(0, textureMur);
-
-	//animator
-	ISceneNodeAnimator* animator = smgr->createRotationAnimator(vector3df(0, 0.1f, 0));
-	cube->addAnimator(animator);
-	animator->drop();
-	animator = 0;
-
-	Prout* p = new Prout();
-	int v = p->getTrenteDeux();
-	int a = p->getNimp();
+	auto p = new Prout();
+	s32 v = p->getTrenteDeux();
+	s32 a = p->getNimp();
 	p->nimp();
 
 	ISceneCollisionManager* collManager = smgr->getSceneCollisionManager();
 
-	//test terrain
-	/*
-	ITerrainSceneNode* terrain = smgr->addTerrainSceneNode("Img/t64.png" ,	//map
-		0,	//parent
-		-1,	//id
-		vector3df(-16,-2,-16),		//position
-		vector3df(0, 0, 0),		//rotation
-		vector3df(4.f, 1.f/2.f, 4.f),	//scale
-		SColor(255, 255, 255, 255),
-		5,
-		ETPS_17,
-		0	//smooth factor
-	);
-	terrain->scaleTexture(32.0f ,32.0f);
-	terrain->setMaterialFlag(E_MATERIAL_FLAG::EMF_LIGHTING, false);
-	terrain->setMaterialTexture(0, textureSol);
-	
-	terrain->setMaterialType(E_MATERIAL_TYPE::EMT_DETAIL_MAP); */
+	//light
+	smgr->setAmbientLight(SColor(255, 255, 255, 255));
+	driver->setAmbientLight(SColor(255, 100, 255, 255));
 
 	//test obj
-	IAnimatedMesh* meshBlob = smgr->getMesh("Map/blob.obj");
+	IAnimatedMesh* meshBlob = smgr->getMesh("Map/blob2.obj");
 	ISceneNode * complix = smgr->addMeshSceneNode(meshBlob);
 	complix->setPosition(vector3df(0, 0, 0));
-	complix->setMaterialFlag(E_MATERIAL_FLAG::EMF_LIGHTING, false);
+	complix->setMaterialFlag(E_MATERIAL_FLAG::EMF_LIGHTING, true);
+	complix->setMaterialFlag(E_MATERIAL_FLAG::EMF_FOG_ENABLE, true);
 	//complix->setMaterialTexture(0, textureMur);
 	//complix->setScale(vector3df(1, 1, 1) / 16);
 
@@ -181,13 +150,21 @@ int main() {
 
 	device->getCursorControl()->setVisible(false);
 
+          
 
-	while (device->run()) {
+	bool alive = true;
+
+	while (device->run() && alive) {
 		//tmp
 		driver->beginScene(true, true, SColor(255, 100, 101, 140));
 		smgr->drawAll();
 		guienv->drawAll();
 		driver->endScene();
+
+		if (camera->getPosition().Y < -300.f) {
+			alive = false;
+		}
+
 	}
 
 	device->drop();
